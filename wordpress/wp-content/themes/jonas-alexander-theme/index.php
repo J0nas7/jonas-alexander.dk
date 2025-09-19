73f2873e4eda9b0676b873aa178226d125acc504
J0nas7 <jonas.sorensen.93dk@gmail.com>
2025-09-19 10:14:28 +0200

    Changing my blog post from a WP page type to a post type

--- wordpress/wp-content/themes/jonas-alexander-theme/index.php
+++ wordpress/wp-content/themes/jonas-alexander-theme/index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
+<?php
+$title = wp_title('', false);
+if ($title) {
+    $title .= " - ";
+}
+?>
+
 <head>
     <base href="/" />
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <title><?php bloginfo('name'); ?><?php wp_title('&mdash;'); ?></title>
+    <title><?= $title; ?><?php bloginfo('name'); ?></title>
     <?php if (is_singular() && get_option('thread_comments')) wp_enqueue_script('comment-reply'); ?>
     <?php wp_head(); ?>
     <link rel="stylesheet" type="text/css" media="all" href="<?= get_template_directory_uri(); ?>/cv.min.css" />
